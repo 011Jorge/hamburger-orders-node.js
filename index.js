22d060e821b51afceb2d4398cbdcb1c6a5dc1a94
011Jorge <vicentejorge187@gmail.com>
2021-12-30 22:48:39 -0300

    front-en conectade or back-end

--- index.js
+++ index.js
@@ -1,88 +1,106 @@
-const { request, response } = require("express")
-const express = require("express")
-const uuid = require("uuid")
+const { request, response } = require("express");
+const express = require("express");
+const uuid = require("uuid");
+const cors = require("cors");
 
-const port = 3000
+const port = 3001;
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
+
+/* Política de CORS autorizada*/
+app.use(cors());
 
 /*! Important ! variable responsible for listing orders */
-const customerOrders = []
+const customerOrders = [];
 
 /*Middlewares responsible for verifying real id*/
 const checkUserId = (request, response, next) => {
-    const { id } = request.params
-    const index = customerOrders.findIndex(orderPlaced => orderPlaced.id === id)
+  const { id } = request.params;
+  const index = customerOrders.findIndex(
+    (orderPlaced) => orderPlaced.id === id
+  );
 
-    if (index < 0) return response.status(404).json({ message: "User not found" })
+  if (index < 0)
+    return response.status(404).json({ message: "User not found" });
 
-    request.orderIndex = index
-    request.orderId = id
+  request.orderIndex = index;
+  request.orderId = id;
 
-    next()
-}
+  next();
+};
 
 const method = (request, response, next) => {
-    console.log(request.method)
-    next()
-}
+  console.log(request.method);
+  next();
+};
 
-app.get('/orders', method, (request, response) => {
-    return response.json(customerOrders)
-})
+app.get("/orders", method, (request, response) => {
+  return response.json(customerOrders);
+});
 
-app.post('/orders', method, (request, response) => {
-    const { order, clientName, price } = request.body
-    const orderPlaced = { id: uuid.v4(), order, clientName, price, status: "Em preparação" }
+app.post("/orders", method, (request, response) => {
+  const { order, clientName, price } = request.body;
+  const orderPlaced = {
+    id: uuid.v4(),
+    order,
+    clientName,
+    price,
+    status: "Em preparação",
+  };
 
-    customerOrders.push(orderPlaced)
+  customerOrders.push(orderPlaced);
 
-    return response.status(201).json(customerOrders)
-})
+  return response.status(201).json(orderPlaced);
+});
 
-app.put('/orders/:id', method, checkUserId, (request, response) => {
-    const { order, clientName, price } = request.body
-    const index = request.orderIndex
-    const id = request.orderId
+app.put("/orders/:id", method, checkUserId, (request, response) => {
+  const { order, clientName, price } = request.body;
+  const index = request.orderIndex;
+  const id = request.orderId;
 
-    const orderUpdate = { id, order, clientName, price, status: "Em preparação" }
+  const orderUpdate = { id, order, clientName, price, status: "Em preparação" };
 
-    customerOrders[index] = orderUpdate
+  customerOrders[index] = orderUpdate;
 
-    return response.json(orderUpdate)
-})
+  return response.json(orderUpdate);
+});
 
-app.delete('/orders/:id', method, checkUserId, (request, response) => {
-    const id = request.orderId
-    const index = request.orderIndex
+app.delete("/orders/:id", method, checkUserId, (request, response) => {
+  const id = request.orderId;
+  const index = request.orderIndex;
 
-    customerOrders.splice(index, 1)
+  customerOrders.splice(index, 1);
 
-    return response.status(204).json(customerOrders)
-})
+  return response.status(204).json(customerOrders);
+});
 
-app.get('/exactOrder/:id', method, checkUserId, (request, response) => {
-    const id = request.orderId
-    const index = request.orderIndex
+app.get("/exactOrder/:id", method, checkUserId, (request, response) => {
+  const id = request.orderId;
+  const index = request.orderIndex;
 
-    const exactOrder = customerOrders[index]
+  const exactOrder = customerOrders[index];
 
-    return response.json(exactOrder)
-})
+  return response.json(exactOrder);
+});
 
-app.patch('/orders/:id', method, checkUserId, (request, response) => {
-    const { order, clientName, price } = request.body
-    const index = request.orderIndex
-    const id = request.orderId
+app.patch("/orders/:id", method, checkUserId, (request, response) => {
+  const { order, clientName, price } = request.body;
+  const index = request.orderIndex;
+  const id = request.orderId;
 
-    const updateOrderPatch = { id, order, clientName, price, status: "Pedido Finalizado" }
-    customerOrders[index] = updateOrderPatch
-    console.log(updateOrderPatch)
+  const updateOrderPatch = {
+    id,
+    order,
+    clientName,
+    price,
+    status: "Pedido Finalizado",
+  };
+  customerOrders[index] = updateOrderPatch;
 
-    return response.json(updateOrderPatch)
-})
+  return response.json(updateOrderPatch);
+});
 
 app.listen(port, () => {
-    console.log("🍔 RUN")
-})
+  console.log("🍔 RUN");
+});
